236300f713ced0ee1a5f29cc8a5b2e0215eca55c
alam <a.lam@optergy.com>
2019-08-22 16:50:11 +0930

    add default login, home and page not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+
+
+import { RouterModule, Routes } from  '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {MatCardModule} from '@angular/material/card';
+import { HomeComponent } from './home/home.component';
+
+const  appRoutes:  Routes  = [
+  {
+    path: 'login',
+    component:  LoginComponent
+  },
+  { path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true}
+    ),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ }
